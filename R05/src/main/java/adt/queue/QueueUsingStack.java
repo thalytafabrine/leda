e23f26ab981a4ed90527f58bc431ee0d21238ea8
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-11-26 17:15:57 -0300

    Adicionando QueueUsingStack ao roteiro 5

---
+++ R05/src/main/java/adt/queue/QueueUsingStack.java
@@ -0,0 +1,69 @@
+package adt.queue;
+
+import adt.stack.Stack;
+import adt.stack.StackImpl;
+import adt.stack.StackOverflowException;
+
+public class QueueUsingStack<T> implements Queue<T> {
+	
+	private Stack<T> stack1;
+    private Stack<T> stack2;
+
+    public QueueUsingStack(int size) {
+    	stack1 = new StackImpl<T>(size);
+    	stack2 = new StackImpl<T>(size);
+    	}
+
+    @Override
+    public void enqueue(T element) throws QueueOverflowException {
+    	try {
+    		stack1.push(element);
+   		} catch (StackOverflowException e) {
+   			throw new QueueOverflowException();
+   		}
+    }
+
+    @Override
+    public T dequeue() throws QueueUnderflowException {
+    	if (stack1.isEmpty())
+    		throw new QueueUnderflowException();
+
+    	try {
+    		while (!stack1.isEmpty())
+    			stack2.push(stack1.pop());
+    		T element = stack2.pop();
+    		while (!stack2.isEmpty())
+    			stack1.push(stack2.pop());
+    		return element;
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return null;
+    }
+
+    @Override
+    public T head() {
+    	if(isEmpty()) return null;
+    	try {
+    		while (!stack1.isEmpty())
+    			stack2.push(stack1.pop());
+    		T element = stack2.top();
+    		while (!stack2.isEmpty())
+    			stack1.push(stack2.pop());
+    		return element;
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return null;
+	}
+
+    @Override
+    public boolean isEmpty() {
+    	return stack1.isEmpty();
+	}
+
+    @Override
+	public boolean isFull() {
+    	return stack1.isFull();
+    }
+}
